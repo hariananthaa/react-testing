30c160eeb913d90c28d55ea0ec8043bdd2c5149f
HARIHARAN <hariharan.k@wealthmindcreation.com>
2023-08-15 10:50:35 +0530

    Code Coverage

--- src/components/greet/greet.test.tsx
+++ src/components/greet/greet.test.tsx
@@ -8,15 +8,15 @@ import Greet from "./greet";
 describe("Greet", () => {
   test("Great renders correctly", () => {
     render(<Greet />);
-    const textElement = screen.getByText("Hello");
+    const textElement = screen.getByText(/Hello/);
     expect(textElement).toBeInTheDocument();
   });
 
-  describe("Nested", () => {
-    test("Greet renders with a name", () => {
-      render(<Greet name="Hari" />);
-      const textElement = screen.getByText("Hello Hari");
-      expect(textElement).toBeInTheDocument();
-    });
-  });
+  // describe("Nested", () => {
+  //   test("Greet renders with a name", () => {
+  //     render(<Greet name="Hari" />);
+  //     const textElement = screen.getByText("Hello Hari");
+  //     expect(textElement).toBeInTheDocument();
+  //   });
+  // });
 });
