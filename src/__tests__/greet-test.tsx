8a0c27e4e7d7696ad8018cf397409d2b98ddfa89
HARIHARAN <hariharan.k@wealthmindcreation.com>
2023-08-15 10:28:22 +0530

    Filename Conventions

---
+++ src/__tests__/greet-test.tsx
@@ -0,0 +1,23 @@
+/**
+ * Great should render the test hello and if a name is passed into the component, it should render hello followed by name.
+ */
+
+import { render, screen } from "@testing-library/react";
+import Greet from "../components/greet/greet";
+
+describe("Greet", () => {
+  // "it" is also same as "test". x means skip
+  xit("Great renders correctly", () => {
+    render(<Greet />);
+    const textElement = screen.getByText("Hello");
+    expect(textElement).toBeInTheDocument();
+  });
+
+  describe("Nested", () => {
+    test("Greet renders with a name", () => {
+      render(<Greet name="Hari" />);
+      const textElement = screen.getByText("Hello Hari");
+      expect(textElement).toBeInTheDocument();
+    });
+  });
+});
