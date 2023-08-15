753200dbc38bb0b487d82a8f93751c900c3c9f21
HARIHARAN <hariharan.k@wealthmindcreation.com>
2023-08-15 10:20:38 +0530

    grouping test

--- src/components/greet/greet.test.tsx
+++ src/components/greet/greet.test.tsx
@@ -2,17 +2,21 @@
  * Great should render the test hello and if a name is passed into the component, it should render hello followed by name.
  */
 
-import { render, screen } from "@testing-library/react"
-import Greet from "./greet"
+import { render, screen } from "@testing-library/react";
+import Greet from "./greet";
 
-test("Great renders correctly",()=>{
-  render(<Greet/>)
-  const textElement = screen.getByText("Hello")
-  expect(textElement).toBeInTheDocument();
-})
+describe("Greet", () => {
+  test("Great renders correctly", () => {
+    render(<Greet />);
+    const textElement = screen.getByText("Hello");
+    expect(textElement).toBeInTheDocument();
+  });
 
-test("Greet renders with a name",()=>{
-  render(<Greet name="Hari"/>)
-  const textElement = screen.getByText("Hello Hari")
-  expect(textElement).toBeInTheDocument();
-})
+  describe("Nested", () => {
+    test("Greet renders with a name", () => {
+      render(<Greet name="Hari" />);
+      const textElement = screen.getByText("Hello Hari");
+      expect(textElement).toBeInTheDocument();
+    });
+  });
+});
